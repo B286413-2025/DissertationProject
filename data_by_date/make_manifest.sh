5cd88334c6e51258803e85b87a9990de8f396ed5
B286413-2025 <snumber@sms.ed.ac.uk>
2026-05-25 12:14:04 +0100

    QIIME2 manifest for all samples per batch

--- data_by_date/make_manifest.sh
+++ data_by_date/make_manifest.sh
@@ -15,29 +15,15 @@ manifest="${3:-$run_dir/fq-manifest.tsv}"
 
 # Relevant codes from subsetting
 stems="$out/stems.txt"
-codes_raw="$out/codes_raw.txt"
-missing="$out/missing.txt"
-
-# Activation codes present in metadata for this run
-comm -23 "$codes_raw" "$missing" > "$out/codes_ok.txt"
 
 # Header line
 echo -e "sample-id\tforward-absolute-filepath\treverse-absolute-filepath" > "$manifest"
 
 while read -r stem; do
-  # Activation code from stem
-  code="${stem:0:6}"
-  # Verifying code present in the current run
-  grep -qx "$code" "$out/codes_ok.txt" || continue
-
-  # Getting the forward and reverse paths
+  # Getting the forward and reverse paths for each sample
   # TODO: add the eddie path
   f="$run_dir/raw_data/${stem}"_R1*.fastq.gz
   r="$run_dir/raw_data/${stem}"_R2*.fastq.gz
   
-  printf "%s\t%s\t%s\n" \
-	  "$stem" \
-	  "$(realpath $f)" \
-	  "$(realpath $r)" \
-	  >> "$manifest"
+  printf "%s\t%s\t%s\n" "$stem" "$(realpath $f)" "$(realpath $r)" >> "$manifest"
 done < "$stems"
