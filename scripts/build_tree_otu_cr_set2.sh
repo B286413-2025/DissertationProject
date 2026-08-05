25096909d5fd9d9866e4e34f90ec766e8b2ef6b9
B286413-2025 <snumber@sms.ed.ac.uk>
2026-08-05 19:17:31 +0100

    Tree building scripts for OTU pipelines and update to full_process

---
+++ scripts/build_tree_otu_cr_set2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+#$ -N build_tree_cr
+#$ -cwd
+#$ -l h_rt=06:00:00
+#$ -pe sharedmem 12
+#$ -o ../logs/$JOB_NAME.out
+#$ -e ../logs/$JOB_NAME.err
+
+set -euo pipefail
+
+mkdir -p ../logs
+mkdir -p ../phylogeny_otu_cr_set2
+
+# Activiating conda environment
+. /etc/profile.d/modules.sh
+module load anaconda
+conda activate rachis-qiime2-2026.4
+
+threads="${NSLOTS:-1}"
+
+qiime phylogeny align-to-tree-mafft-fasttree \
+  --i-sequences ../otu_otus_cr_set2/otu97cr_repseq_lenfilt.qza \
+  --p-n-threads "${threads}" \
+  --o-alignment ../phylogeny_otu_cr_set2/otu97cr_aligned.qza \
+  --o-masked-alignment ../phylogeny_otu_cr_set2/otu97cr_masked.qza \
+  --o-tree ../phylogeny_otu_cr_set2/otu97cr_unrooted.qza \
+  --o-rooted-tree ../phylogeny_otu_cr_set2/otu97cr_rooted.qza
